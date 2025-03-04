403d642a9171b1a5afda0a1b1977935343f4a610
quancoi2ka3 <nguyenmanhquan17072003@gmail.com>
2025-03-04 12:04:20 +0700

    update avt

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -11,7 +11,7 @@ export const personalInfo = {
   github: "https://github.com/quancoi2ka3",
   phone: "+84 968 031 328",
   cvLink: "/NguyenManhQuan_CV_dotNET.pdf", // Add your actual CV file
-  avatarUrl: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+  avatarUrl: "../image/avt_profile.jpg",
 };
 
 export const experiences = [
